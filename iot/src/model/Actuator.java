05b3528d8c78ef81f62cbd29c8354ca79d2ee062
victoria <vikymediana@gmail.com>
2017-05-25 21:10:44 +0200

    Nuevo proyecto

---
+++ iot/src/model/Actuator.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by victoria on 25/05/17.
+ */
+public class Actuator extends Item {
+
+}
---
+++ iot/src/model/Item.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by victoria on 25/05/17.
+ */
+public class Item {
+    private String id;
+}
